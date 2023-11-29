913dc0a8652884ec4ecdbcbd2d8c2098aadcea5c
shivani.1108 <shivani@montezuma.ai>
2023-11-29 16:53:44 +0530

    all commit

--- shivani_upadhye_portfolio/src/app/app.component.ts
+++ shivani_upadhye_portfolio/src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router, Scroll } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+@HostListener('window:scroll')
+
 export class AppComponent {
-  title = 'shivani_upadhye_portfolio';
+  title = 'shiv_portfolio';
+
+  constructor(
+    private router: Router
+  ) { }
+  
+ 
+  @HostListener('window:wheel' ,['$event']) onWheelScroll(evento: WheelEvent) {
+    if (evento.deltaY > 0) {
+      
+      switch (this.router.url) {
+        case '/': {
+          this.router.navigate(['/about'])
+          break
+        }
+        case '/about': {
+          this.router.navigate(['/skills'])
+          break
+        }
+        case '/skills': {
+          this.router.navigate(['/contact'])
+          break
+        }
+        case '/contact': {
+          break
+        }
+      }
+    } else { 
+
+      switch (this.router.url) {
+        case '/': {
+          break
+        }
+        case '/about': {
+          this.router.navigate(['/'])
+          break
+        }
+        case '/skills': {
+          this.router.navigate(['/about'])
+          break
+        }
+        case '/contact': {
+          this.router.navigate(['/skills'])
+          break
+        }
+      }
+    }
+  }
 }
--- shivani_upadhye_portfolio/src/app/app.module.ts
+++ shivani_upadhye_portfolio/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
